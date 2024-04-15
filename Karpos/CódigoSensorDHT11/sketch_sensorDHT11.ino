adb32c796a8a2f8ebe14fc285b9953167a4d17f1
Victor Moralles Araujo Paiva <160356771+Vmoralles@users.noreply.github.com>
2024-04-15 11:25:39 -0300

    Codigo Arduino

--- Karpos/CódigoSensorDHT11/sketch_sensorDHT11.ino
+++ Karpos/CódigoSensorDHT11/sketch_sensorDHT11.ino
@@ -1,35 +1,25 @@
-//Processo da declaração de porta: Serve para armazenar os dados que serão recebidos pelo sensor. //
 
+​
+VICTOR MORALLES ARAUJO PAIVA .
+​
 #include "DHT.h"
 #define dht_type DHT11
 
-int dht_pin = A0;
-DHT dht_1 = DHT(dht_pin, dht_type);
-
-// Processo do Setup: Serve para configurar o ambiente inicial do código. //
+int pino_dht11 = A0;
+int pino_lm35 = A5;
+DHT dht_1 = DHT(pino_dht11, dht_type);
 
 void setup() {
   Serial.begin(9600);
   dht_1.begin();
 }
 
-// Processo do Looping: Serve para manter a constância na atualização dos dados recebidos pelo sensor.
-
 void loop() {
-  float umidade = dht_1.readHumidity();
-  float temperatura = dht_1.readTemperature();
-  if (isnan(temperatura) or isnan(umidade)) {
-    Serial.println("Erro ao ler");
-  } else {
-    
+  float umidade = dht_1.readHumidity() + 10;
+  float temperatura = analogRead(pino_lm35) * 0.00488 * 100;
+
     Serial.print(umidade);
-
-    Serial.print("  ");
-    
+    Serial.print(";");
     Serial.println(temperatura);
-
-    
-
-  }
-  delay(2000);
+    delay(1000);
 }
