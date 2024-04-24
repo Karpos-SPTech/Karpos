104f023353f79e061118d0533712ecaae27b7741
unknown <mauricio.almeida@sptech.schoo>
2024-04-24 10:54:03 -0300

    Bd oficial

--- Projeto/BD_Oficial.sql
+++ Projeto/BD_Oficial.sql
@@ -9,61 +9,64 @@ CREATE TABLE cadastro (
     email VARCHAR(100) NOT NULL UNIQUE,
     senha VARCHAR(30) NOT NULL,
     documento VARCHAR(14) UNIQUE,
-    cargo VARCHAR(14),
-	telefone VARCHAR(11),
-CONSTRAINT chkCargo CHECK (cargo IN('funcionario','administrador'))
-);
-
-CREATE TABLE login (            
-    idUsuario INT PRIMARY KEY AUTO_INCREMENT,
-    email VARCHAR(100) NOT NULL UNIQUE,
-    senha VARCHAR(30) NOT NULL,
-	fkCadastro INT, CONSTRAINT fkCadastroLogin
-    FOREIGN KEY(fkCadastro) REFERENCES Cadastro(idCadastro),
-	fkFazenda INT, CONSTRAINT fkFazendaLogin
-    FOREIGN KEY(fkFazenda) REFERENCES fazenda(idFazenda)
-);
-
-CREATE TABLE sensor (
-	idSensor INT PRIMARY KEY AUTO_INCREMENT,
-    temperatura DECIMAL(4,2),
-	umidade FLOAT,
-    fkTerreno int, CONSTRAINT fkFazendaSensor
-    FOREIGN KEY(fkFazenda) REFERENCES fazenda(idFazenda)
-);
-
-DESC sensor;
-    
--- Tabela responsavel por armazenar os dados da fazenda
-CREATE TABLE fazenda (
-	idFazenda INT PRIMARY KEY AUTO_INCREMENT,
-	endereco VARCHAR(50) NOT NULL,
-	cep CHAR(8) NOT NULL,
-	Hectares FLOAT NOT NULL,
-	qtdSensores INT NOT NULL
+	telefone VARCHAR(11)
 );
 
 INSERT INTO cadastro VALUES
-(default,'Fernanda Caramico','fernanda.caramico@sptech.school','fern4nd4c4r4m1c0','12345678900','administrador','11924356879'),
-(default,'Mauricio Almeida','mauricio.almeida@sptech.school','m4ur1c104lmeid4','12345678901','administrador','11924356878'),
-(default,'Alexandre Frizzon','alexandre.frizzon@sptech.school','fern4nd4c4r4m1c0','12345678902','funcionario','11924356877');
+(default,'Fernanda Caramico','fernanda.caramico@sptech.school','fern4nd4c4r4m1c0','12345678900','11924356879'),
+(default,'Mauricio Almeida','mauricio.almeida@sptech.school','m4ur1c104lmeid4','12345678901','11924356878'),
+(default,'Alexandre Frizon','alexandre.frizzon@sptech.school','fern4nd4c4r4m1c0','12345678902','11924356877');
 
-
+    CREATE TABLE login (            
+    idUsuario INT AUTO_INCREMENT,
+    fkCadastro INT, constraint pkCadastro PRIMARY KEY(idUsuario,fkCadastro),
+    email VARCHAR(100) NOT NULL UNIQUE,
+    senha VARCHAR(30) NOT NULL,
+	CONSTRAINT fkCadastroLogin
+    FOREIGN KEY(fkCadastro) REFERENCES Cadastro(idCadastro)
+);
 INSERT INTO login VALUES
-INSERT INTO sensor VALUES
+(default,1,'fernanda.caramico@sptech.school','fern4nd4c4r4m1c0'),
+(default,2,'mauricio.almeida@sptech.school','mauricioAlm3id4'),
+(default,3,'alexandre.frizon@sptech.school','alefr11z0n');
+
+select * from login join cadastro on login.fkCadastro = cadastro.idCadastro;
+
+-- Tabela responsavel por armazenar os dados da fazenda
+CREATE TABLE fazenda (
+	idFazenda INT PRIMARY KEY AUTO_INCREMENT,
+    nomeFazenda varchar(45),
+	cep CHAR(9) NOT NULL,
+    numEnd varchar(45),
+	Hectares FLOAT NOT NULL,
+	qtdSensores INT NOT NULL,
+    	fkUsuário INT, CONSTRAINT fkFazendaLogin
+    FOREIGN KEY(fkUsuário) REFERENCES login(idUsuario)
+);
 INSERT INTO fazenda VALUES
+(default,'Fazenda Caramico','08234-350','335',5.5,10,1),
+(default,'Fazenda Almeida','07639-345','490A',4,3,2),
+(default,'Fazenda Frizon','09235-230','90C',3.6,20,3);
 
+CREATE TABLE sensor (
+	idSensor INT AUTO_INCREMENT,
+	fkTerreno int, constraint fkTerrenoSensor PRIMARY KEY(idSensor,fkTerreno),
+    temperatura DECIMAL(4,2),
+	umidade FLOAT,
+    CONSTRAINT fkFazendaSensor
+    FOREIGN KEY(fkTerreno) REFERENCES fazenda(idFazenda)
+);
 
-SELECT concat(nome, email) FROM produtor;
+create table parametros(
+idParametro int primary key auto_increment,
+espécie varchar(45),
+tempMax float,
+tempMin float,
+UmidadeMax float,
+UmidadeMin float);
 
-SELECT concat(nome, ' ', email) FROM produtor;
+SELECT concat('O(a) produtor(a) ', nome, ' possui o email ', email) 
+FROM cadastro;
 
-SELECT concat('O produtor ', nome, ' possui o email ', email) 
-FROM produtor;
-    
-SELECT concat(nome, email) FROM usuario;
-
-SELECT concat(nome, ' ', email) FROM produtor;
-
-SELECT concat('O usuario ', nome, ' possui o nivel de acesso: ', nivel_de_acesso) 
-FROM usuario;
+SELECT concat('Atualmente, sua temperatura se encontra em ', temperatura, ', e a umidade está em:', umidade) 
+FROM sensor;
