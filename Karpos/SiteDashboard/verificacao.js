6666b6207726523f59851051efc91fb463bc1fa5
Vmoralles <vitor.paiva@sptech.school>
2024-04-29 13:47:44 -0300

    Update Alerts registro-login

--- Karpos/SiteDashboard/verificacao.js
+++ Karpos/SiteDashboard/verificacao.js
@@ -1,9 +1,10 @@
 function registrar() {
     const senha = input_senha.value
-    const telefone = Number(input_telefone.value)
+    const telefone = input_telefone.value
     const email = input_email.value
     const senhaConfirmada = input_senhaConfirmada.value
     const documento = input_documento.value
+    const documentacao = select_doc.value
     let verificarLetraMaiuscula = false
     let verificarCaracterEspecial = false
     let caracteresEspeciais = "!@#$%^&*(),.?/:{}|<>"
@@ -18,43 +19,70 @@ function registrar() {
         telefone == "" ||
         senhaConfirmada == "" ||
         documento == "") {
-        div_alertasValidacao.innerHTML += "PREENCHA TODOS OS CAMPOS"
-    }
-    // verificação de senha
-    else if (senha) {
-        if (senha < 8) {
-            alert("A senha tem que ter no minimo 8 caracteres")
-        } 
-        // verificação de caracter especial + letra maiuscula + for
-        else {
-            for (let senhaVerificiar = 0; senhaVerificiar < senha.length; senhaVerificiar++) {
-                let char = senha[senhaVerificiar]
-                if (caracteresEspeciais.indexOf(char) !== -1) {
-                    verificarCaracterEspecial = true;
-                }
-                if (letrasMaiusculas.indexOf(char) !== -1) {
-                    verificarLetraMaiuscula = true;
-                }
-            }
-            if (verificarCaracterEspecial && verificarLetraMaiuscula) {
-                senhaValidada = true
-            }
+        div_paiAlertas.style.display = 'block';
+        div_alertasValidacao.innerHTML = "PREENCHA TODOS OS CAMPOS!"
 
-            if (senhaValidada == false) {
-                // alert("Digite um senha com caracteres especial e letra maiscula")
-            }
-        }
     }
     // verificação telefone
-    else if (telefone < 12) {
-        alert("Telefone não esta completo")
+    else if (telefone.length < 11 || telefone.length > 11) {
+        div_paiAlertas.style.display = 'block';
+        div_alertasValidacao.innerHTML = "O telefone não esta completo!"
     }
     // verificação senha igual
     else if (senha != senhaConfirmada) {
-        alert("as senhas tem que ser ingual")
+        div_paiAlertas.style.display = 'block';
+        div_alertasValidacao.innerHTML = "As senhas precisam ser iguais!"
     }
     // verificação email
-    else if (email.indexOF("@") == -1 || email.indexOF(".") == -1) {
-        alert("digite um email valido")
+    else if (email.indexOf("@") == -1 || email.indexOf(".") == -1) {
+        div_paiAlertas.style.display = 'block';
+        div_alertasValidacao.innerHTML = "Digite um e-mail válido!"
+    } else if (documentacao == "cpf" && documento.length < 11) {
+        div_paiAlertas.style.display = 'block';
+        div_alertasValidacao.innerHTML = "O CPF deve ter no mínimo 11 dígitos";
+    } else if (documentacao == "rg" && documento.length < 9) {
+        div_paiAlertas.style.display = 'block';
+        div_alertasValidacao.innerHTML = "O RG deve ter no mínimo 9 dígitos";
+    } else if (documentacao == "cnpj" && documento.length < 14) {
+        div_paiAlertas.style.display = 'block';
+        div_alertasValidacao.innerHTML = "O CNPJ deve ter no mínimo 14 dígitos";
+    } else if (documentacao == "rne" && documento.length < 7) {
+        div_paiAlertas.style.display = 'block';
+        div_alertasValidacao.innerHTML = "O RNE deve ter no mínimo 7 dígitos";
     }
+    // verificação de senha
+    else if (senha.length < 8) {
+        div_paiAlertas.style.display = 'block';
+        div_alertasValidacao.innerHTML = "A senha tem que ter no minimo 8 caracteres"
+    }
+    // verificação de caracter especial + letra maiuscula + for
+    else {
+        for (let senhaVerificiar = 0; senhaVerificiar < senha.length; senhaVerificiar++) {
+            let char = senha[senhaVerificiar]
+            if (caracteresEspeciais.indexOf(char) != -1) {
+                verificarCaracterEspecial = true;
+            }
+            if (letrasMaiusculas.indexOf(char) != -1) {
+                verificarLetraMaiuscula = true;
+            }
+        }
+        if (verificarCaracterEspecial && verificarLetraMaiuscula) {
+            senhaValidada = true
+        }
+
+        if (senhaValidada == false) {
+            div_paiAlertas.style.display = 'block';
+            div_alertasValidacao.innerHTML = "Digite um senha com caracteres especial e letra maiscula"
+        } else {
+            window.location.href = "login.html";
+        }
+    }
+}
+
+function login() {
+    window.location.href = "index.html";
+}
+
+function exit() {
+    window.location.href = "tela_inicial_dashb.html";
 }
