b076cb21fae712d98bcb67470a523814ab7681c1
MauricioAlmeida03 <mauricio.almeida@sptech.school>
2024-05-04 10:35:47 -0300

    Banco de Dados Karpos

--- Banco de Dados/BD_Oficial.sql
+++ Banco de Dados/BD_Oficial.sql
@@ -1,74 +1,139 @@
-CREATE DATABASE KarposTech;
+CREATE DATABASE karposTech;
 
-USE KarposTech;
+USE karposTech;
+create table empresa(
+idEmpresa int primary key auto_increment,
+nome varchar(45),
+cnpj char(14),
+cep CHAR(9),
+numEnd varchar(45),
+complemento varchar(45));
+
+insert into empresa values
+(default,'Caramico Cloud','01456963849','03560-340','450A','Jardim Santa Helena'),
+(default,'Karpos Ltda','85738693019','04980-400','320','Bairro do Limoeiro');
 
 -- Tabela que armazenara os dados do produtor de algodão
-CREATE TABLE cadastro (
-    idCadastro INT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(50) NOT NULL,
-    email VARCHAR(100) NOT NULL UNIQUE,
-    senha VARCHAR(30) NOT NULL,
-    documento VARCHAR(14) UNIQUE,
-	telefone VARCHAR(11)
+create table usuario (
+    idUsuario  int primary key auto_increment,
+    nome varchar(50) not null,
+    email varchar(100) not null unique,
+    senha varchar(30) not null,
+    documento varchar(14) unique,
+	telefone varchar(11),
+    fkEmpresa int, constraint fkEmpresaUsuario
+    foreign key(fkEmpresa) references empresa(idEmpresa)
 );
 
-INSERT INTO cadastro VALUES
-(default,'Fernanda Caramico','fernanda.caramico@sptech.school','fern4nd4c4r4m1c0','12345678900','11924356879'),
-(default,'Mauricio Almeida','mauricio.almeida@sptech.school','m4ur1c104lmeid4','12345678901','11924356878'),
-(default,'Alexandre Frizon','alexandre.frizzon@sptech.school','fern4nd4c4r4m1c0','12345678902','11924356877');
+insert into usuario values
+(default,'Fernanda Caramico','fernanda.caramico@sptech.school','fern4nd4c4r4m1c0','12345678900','11924356879',1),
+(default,'Mauricio Almeida','mauricio.almeida@sptech.school','m4ur1c104lmeid4','12345678901','11924356878',2);
 
-    CREATE TABLE login (            
-    idUsuario INT AUTO_INCREMENT,
-    fkCadastro INT, constraint pkCadastro PRIMARY KEY(idUsuario,fkCadastro),
-    email VARCHAR(100) NOT NULL UNIQUE,
-    senha VARCHAR(30) NOT NULL,
-	CONSTRAINT fkCadastroLogin
-    FOREIGN KEY(fkCadastro) REFERENCES Cadastro(idCadastro)
-);
-INSERT INTO login VALUES
-(default,1,'fernanda.caramico@sptech.school','fern4nd4c4r4m1c0'),
-(default,2,'mauricio.almeida@sptech.school','mauricioAlm3id4'),
-(default,3,'alexandre.frizon@sptech.school','alefr11z0n');
 
-select * from login join cadastro on login.fkCadastro = cadastro.idCadastro;
 
--- Tabela responsavel por armazenar os dados da fazenda
-CREATE TABLE fazenda (
-	idFazenda INT PRIMARY KEY AUTO_INCREMENT,
-    nomeFazenda varchar(45),
-	cep CHAR(9) NOT NULL,
-    numEnd varchar(45),
-	Hectares FLOAT NOT NULL,
-	qtdSensores INT NOT NULL,
-    	fkUsuário INT, CONSTRAINT fkFazendaLogin
-    FOREIGN KEY(fkUsuário) REFERENCES login(idUsuario)
-);
-INSERT INTO fazenda VALUES
-(default,'Fazenda Caramico','08234-350','335',5.5,10,1),
-(default,'Fazenda Almeida','07639-345','490A',4,3,2),
-(default,'Fazenda Frizon','09235-230','90C',3.6,20,3);
-
-CREATE TABLE sensor (
-	idSensor INT AUTO_INCREMENT,
-	fkTerreno int, constraint fkTerrenoSensor PRIMARY KEY(idSensor,fkTerreno),
-    umidade FLOAT,
-    temperatura DECIMAL(4,2),
-    CONSTRAINT fkFazendaSensor
-    FOREIGN KEY(fkTerreno) REFERENCES fazenda(idFazenda)
-);
 create table parametros(
 idParametro int primary key auto_increment,
-espécie varchar(45),
-tempMax float,
+tempIdealMin float,
+tempIdealMax float,
 tempMin float,
+tempMax float,
 UmidadeMax float,
-UmidadeMin float);
+UmidadeMin float
+)auto_increment=100;
 
-drop table sensor;
+insert into parametros values
+(default,25,30,16,34,60,70);
+
+-- Tabela responsavel por armazenar os dados da fazenda
+create table fazenda (
+	idFazenda int primary key auto_increment,
+    nome varchar(45),
+	cep char(9) not null,
+    numEnd varchar(45),
+	hectares float not null,
+	qtdSensores int not null,
+    	fkEmpresa int, constraint fkEmpresaFazenda
+    foreign key(fkEmpresa) references empresa(idEmpresa),
+    
+		fkParametro int, constraint fkFazendaParametro
+	foreign key (fkParametro) references parametros(idParametro)
+);
+insert into fazenda values
+(default,'Cotton Farm 1','08234-350','335',5,10,1,100),
+(default,'Cotton Farm','07560-432','960B',8,15,1,100),
+(default,'Fazenda Almeida','07639-345','490A',4,3,2,100);
+
+select * from fazenda;
+
+CREATE TABLE sensor (
+	idSensor int auto_increment,
+    tipo varchar(45), constraint chkTipo
+    check (tipo in('dht11Umidade','lm35Temperatura')),
+	fkFazenda int, constraint fkTerrenoSensor primary key(idSensor,fkFazenda),
+    constraint fkFazendaSensor
+    foreign key(fkFazenda) references fazenda(idFazenda)
+);
+
+insert into sensor values
+(default,'lm35Temperatura',1),
+(default,'lm35Temperatura',1),
+(default,'dht11Umidade',1);
+
+create table dados(
+idDados int auto_increment,
+fkSensor int, primary key(idDados,fkSensor),
+dtHorario datetime,
+valorSensor decimal (5,2),
+valor decimal(4,2),
+constraint fkdadosSensor foreign key (fkSensor)
+references sensor(idSensor));
+
+insert into dados values
+(default,1,'2024-05-03 14:00:00',30.05,1),
+(default,1,'2024-05-03 15:00:00',25.40,1),
+(default,1,'2024-05-03 16:00:00',26.00,1),
+(default,1,'2024-05-03 16:30:00',24.90,1),
+(default,1,'2024-05-03 16:40:00',19.55,1),
+(default,2,'2024-05-04 19:00:00',28.12,0.8),
+(default,2,'2024-05-04 19:10:00',32.10,0.8),
+(default,2,'2024-05-06 10:40:00',30.20,0.8),
+(default,3,'2024-05-04 15:30:00',74.10,1),
+(default,3,'2024-05-04 15:52:00',60.10,1),
+(default,3,'2024-05-05 11:20:00',65.40,1),
+(default,3,'2024-05-05 11:22:00',64.90,1);
+
+insert into dados values
+(default,1,'2024-05-03 14:00:00',38.05,1),
+(default,1,'2024-05-03 14:00:00',39.40,1),
+(default,2,'2024-05-03 14:00:00',15.12,1);
+
+select * from dados;
+
 SELECT concat('O(a) produtor(a) ', nome, ' possui o email ', email) 
-FROM cadastro;
-
-SELECT concat('Atualmente,a umidade está em:', umidade, ', e a temperatura do plantio se encontra em: ', temperatura) 
-FROM sensor;
+FROM usuario;
 
 select * from sensor;
+
+create view captura as
+select s.idSensor , s.tipo, d.dtHorario as 'Horario de captura', round(d.valorSensor*d.valor,2) as Valor from dados as d join sensor as s order by idSensor;
+
+create view Estavel as
+SELECT  p.tempMin as tempMin,d.valorSensor as valor, p.tempMax as tempMax
+FROM parametros as p join dados as d
+WHERE valorSensor >= tempMin
+AND valorSensor <= tempMax;
+
+create view Alerta as
+SELECT  d.fkSensor as sensor, p.tempMin as tempMin,d.valorSensor as valor, p.tempMax as tempMax
+FROM parametros as p join dados as d
+WHERE d.valorSensor <= tempMin
+or d.valorSensor >= tempMax and d.fkSensor in(1,2) order by d.fkSensor;
+
+-- Trás todos os dados que estão dentro dos parâmetros estabelecidos
+select * from Estavel;
+
+-- Trás todos os dados que estão fora dos parâmetros estabelecidos
+select * from Alerta;
+
+-- Trás todos os dados capturados, tanto temperatura quanto umidade
+select * from captura;
