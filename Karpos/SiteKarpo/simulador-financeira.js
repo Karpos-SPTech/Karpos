80ec0ff7971df8287e6784d6e73953e4791cbf56
Vmoralles <victor.paiva@sptech.school>
2024-05-09 10:29:58 -0300

    Update Calculadora

--- Karpos/SiteKarpo/simulador-financeira.js
+++ Karpos/SiteKarpo/simulador-financeira.js
@@ -1,4 +1,4 @@
-let myChart; // Variável global para armazenar o gráfico
+let myChart;
 
 document.addEventListener("DOMContentLoaded", function () {
     const botaoSimular = document.querySelector('.botaoSimular');
@@ -7,97 +7,62 @@ document.addEventListener("DOMContentLoaded", function () {
     const botaoFechar = document.querySelector('.fecharSimulacao');
 
     botaoSimular.addEventListener('click', () => {
-        resultadoSimulacao.classList.add('ativa'); // Mostra o resultado da simulação
-        conteudoContato.classList.add('ativa'); // Oculta o conteúdo de contato
+        resultadoSimulacao.classList.add('ativa');
+        conteudoContato.classList.add('ativa');
     });
 
     botaoFechar.addEventListener('click', () => {
-        resultadoSimulacao.classList.remove('ativa'); // Esconde o resultado da simulação
-        conteudoContato.classList.remove('ativa'); // Mostra o conteúdo de contato
+        resultadoSimulacao.classList.remove('ativa');
+        conteudoContato.classList.remove('ativa');
     });
 });
 
 function limpar() {
-    quantidade_perdas_input.value = "";
-    plantioMedio_input.value = "";
-    nome_empresa.value = "";
-    const tituloPaginaContato = document.querySelector('.titulo');
-    const subtituloPaginaContato = document.querySelector('.subtitulo');
+    [quantidade_perdas_input.value, plantioMedio_input.value, nome_empresa.value] = ["", "", ""];
 
-    tituloPaginaContato.textContent = "Bem vindo!";
-    subtituloPaginaContato.textContent = "Preencha o formulário abaixo e faremos um orçamento para a sua empresa";
+    document.querySelector('.titulo').textContent = "Bem vindo!";
+    document.querySelector('.subtitulo').textContent = "Preencha o formulário abaixo e faremos um orçamento para a sua empresa";
 }
 
 function calcular() {
-    let perdaFungoCiclo = Number(quantidade_perdas_input.value);
-    let custoPlantioCiclo = Number(plantioMedio_input.value);
-    let porcentagemRecuperacao = (perdaFungoCiclo / custoPlantioCiclo) * 100;
+    const quantidade_perdas = Number(quantidade_perdas_input.value);
+    const plantio_medio = Number(plantioMedio_input.value);
+    const porcentagem_recuperacao = (quantidade_perdas / plantio_medio) * 100;
 
-    let formatCustoPlantioCiclo = custoPlantioCiclo.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
-    let formatPerdaFungoCiclo = perdaFungoCiclo.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
 
-    const tituloPaginaContato = document.querySelector('.titulo');
-    const subtituloPaginaContato = document.querySelector('.subtitulo');
+    const formatCurrency = (value) => value.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
 
-    tituloPaginaContato.textContent = "Simulação Pronta!";
-    subtituloPaginaContato.textContent = "Aqui está a sua simulação de perdas e o quanto você pode lucrar com a nossa solução";
+    document.querySelector('.titulo').textContent = "Simulação Pronta!";
+    document.querySelector('.subtitulo').textContent = "Aqui está a sua simulação de perdas e o quanto você pode lucrar com a nossa solução";
 
-    const cartaoCustoPlantio = document.querySelector('.cartao:nth-child(1)');
-    const cartaoPerda = document.querySelector('.cartao:nth-child(2)');
-    const cartaoOportunidade = document.querySelector('.cartao:nth-child(3)');
+    document.querySelector('.cartao:nth-child(1) h2').textContent = 'Custo total do Plantio';
+    document.querySelector('.cartao:nth-child(1) p').innerHTML = `<b>${formatCurrency(plantio_medio)}</b>`;
 
-    cartaoCustoPlantio.querySelector('h2').textContent = 'Custo total do Plantio';
-    cartaoCustoPlantio.querySelector('p').innerHTML = `<b>${formatCustoPlantioCiclo}</b>`;
+    document.querySelector('.cartao:nth-child(2) h2').textContent = 'Média de perda por ciclo';
+    document.querySelector('.cartao:nth-child(2) p').innerHTML = `<b style="color:red;">${formatCurrency(quantidade_perdas)}</b>`;
 
-    cartaoPerda.querySelector('h2').textContent = 'Média de perda por ciclo';
-    cartaoPerda.querySelector('p').innerHTML = `<b style="color:red;">${formatPerdaFungoCiclo}</b>`;
+    document.querySelector('.cartao:nth-child(3) h2').textContent = 'Oportunidade';
+    document.querySelector('.cartao:nth-child(3) p').innerHTML = `Olá <b>${nome_empresa.value}</b>, Com base nos dados informados anteriormente, nosso projeto <br>será capaz de <b>reduzir</b> sua perda em até <b>${porcentagem_recuperacao.toFixed(2)}%</b>`;
 
-    cartaoOportunidade.querySelector('h2').textContent = 'Oportunidade';
-    cartaoOportunidade.querySelector('p').innerHTML = `Olá <b>${nome_empresa.value}</b>, Com base nos dados informados anteriormente, nosso projeto <br>será capaz de <b>reduzir</b> sua perda em até <b>${porcentagemRecuperacao.toFixed(2)}%</b>`;
+    const total = 100 - porcentagem_recuperacao;
 
-    const total = 100 - porcentagemRecuperacao;
-
-    // Verifica se o gráfico já foi criado
     if (!myChart) {
         const ctx = document.getElementById('myChart').getContext('2d');
         myChart = new Chart(ctx, {
             type: 'pie',
             data: {
-                labels: ['Custo Total do Plantio (%)', 'Média de Perda por Ciclo (%)'],
+                labels: ['Custo Total do Plantio', 'Média de Perda por Ciclo'],
                 datasets: [{
-                    label: 'Valores',
-                    data: [total, porcentagemRecuperacao],
-                    backgroundColor: [
-                        '#a17f6c',
-                        '#805b46'
-                    ],
-                    borderColor: [
-                        '#745c4f',
-                        '#50392c'
-                    ],
+                    data: [total, porcentagem_recuperacao],
+                    backgroundColor: ['#a17f6c', '#805b46'],
+                    borderColor: ['#745c4f', '#50392c'],
                     borderWidth: 2
                 }]
             },
-            options: {
-                plugins: {
-                    legend: {
-                        labels: {
-                            color: 'black' // Define a cor das labels como vermelho
-                        }
-                    }
-                },
-                scales: {
-                    y: {
-                        beginAtZero: true,
-                        display: false
-                    }
-                }
-            }
+            options: { plugins: { legend: { labels: { color: 'black' } } }, scales: { y: { beginAtZero: true, display: false } } }
         });
     } else {
-        // Atualiza os valores dos dados com os novos valores
-        myChart.data.datasets[0].data = [total, porcentagemRecuperacao];
-        // Atualiza o gráfico
+        myChart.data.datasets[0].data = [total, porcentagem_recuperacao];
         myChart.update();
     }
 }
