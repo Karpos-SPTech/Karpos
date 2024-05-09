0bc9dd9ed23fdbafe96d6d3fbbd9f0a152ca638d
frizzon00 <alexandre.frizon@sptech.school>
2024-05-08 23:36:12 -0300

    Exportação da Tabela

--- Karpos/SiteDashboard/trocarsecao.js
+++ Karpos/SiteDashboard/trocarsecao.js
@@ -94,3 +94,186 @@ var usuario = document.getElementById("section_areaDoUsuario");
     areaTemperatura.classList.remove("active");
     areaUsuario.classList.remove("active");
   });
+
+
+  /*---------------------- Icones dos Lotes -------------------------------*/
+
+  iconTemp1.addEventListener("click", function () {
+    usuario.style.display = "none";
+    temperatura.style.display = "block";
+    umidade.style.display = "none";
+    tabelas.style.display = "none";
+    // filtro.style.zIndex = '10';
+    // referencia.style.zIndex = '10';
+    // // filtro.style.position = 'relative';
+    // referencia.style.position = 'relative';
+
+    temperatura.classList.add("fade");
+    umidade.classList.remove("fade");
+    tabelas.classList.remove("fade");
+    usuario.classList.remove("fade");
+
+    areaTemperatura.classList.add("active");
+    areaUmidade.classList.remove("active");
+    areaTabelas.classList.remove("active");
+    areaUsuario.classList.remove("active");
+  });
+
+  iconTemp2.addEventListener("click", function () {
+    usuario.style.display = "none";
+    temperatura.style.display = "block";
+    umidade.style.display = "none";
+    tabelas.style.display = "none";
+    // filtro.style.zIndex = '10';
+    // referencia.style.zIndex = '10';
+    // // filtro.style.position = 'relative';
+    // referencia.style.position = 'relative';
+
+    temperatura.classList.add("fade");
+    umidade.classList.remove("fade");
+    tabelas.classList.remove("fade");
+    usuario.classList.remove("fade");
+
+    areaTemperatura.classList.add("active");
+    areaUmidade.classList.remove("active");
+    areaTabelas.classList.remove("active");
+    areaUsuario.classList.remove("active");
+  });
+
+  iconTemp3.addEventListener("click", function () {
+    usuario.style.display = "none";
+    temperatura.style.display = "block";
+    umidade.style.display = "none";
+    tabelas.style.display = "none";
+    // filtro.style.zIndex = '10';
+    // referencia.style.zIndex = '10';
+    // // filtro.style.position = 'relative';
+    // referencia.style.position = 'relative';
+
+    temperatura.classList.add("fade");
+    umidade.classList.remove("fade");
+    tabelas.classList.remove("fade");
+    usuario.classList.remove("fade");
+
+    areaTemperatura.classList.add("active");
+    areaUmidade.classList.remove("active");
+    areaTabelas.classList.remove("active");
+    areaUsuario.classList.remove("active");
+  });
+
+  iconTemp4.addEventListener("click", function () {
+    usuario.style.display = "none";
+    temperatura.style.display = "block";
+    umidade.style.display = "none";
+    tabelas.style.display = "none";
+    // filtro.style.zIndex = '10';
+    // referencia.style.zIndex = '10';
+    // // filtro.style.position = 'relative';
+    // referencia.style.position = 'relative';
+
+    temperatura.classList.add("fade");
+    umidade.classList.remove("fade");
+    tabelas.classList.remove("fade");
+    usuario.classList.remove("fade");
+
+    areaTemperatura.classList.add("active");
+    areaUmidade.classList.remove("active");
+    areaTabelas.classList.remove("active");
+    areaUsuario.classList.remove("active");
+  });
+
+  iconUmi1.addEventListener("click", function () {
+    usuario.style.display = "none";
+    temperatura.style.display = "none";
+    umidade.style.display = "block";
+    tabelas.style.display = "none";
+    // filtro.style.zIndex = '10';
+    // referencia.style.zIndex = '10';
+    // // filtro.style.position = 'relative';
+    // referencia.style.position = 'relative';
+
+    // areaRelatorio.classList.remove("fade");
+    // areaUmidade.classList.remove("fade");
+    // areaSuporte.classList.remove("fade");
+    // areaTemperatura.classList.add("fade");
+    temperatura.classList.remove("fade");
+    umidade.classList.add("fade");
+    tabelas.classList.remove("fade");
+    usuario.classList.remove("fade");
+    areaUmidade.classList.add("active");
+    areaTemperatura.classList.remove("active");
+    areaUsuario.classList.remove("active");
+    areaTabelas.classList.remove("active")
+  });
+
+  iconUmi2.addEventListener("click", function () {
+    usuario.style.display = "none";
+    temperatura.style.display = "none";
+    umidade.style.display = "block";
+    tabelas.style.display = "none";
+    // filtro.style.zIndex = '10';
+    // referencia.style.zIndex = '10';
+    // // filtro.style.position = 'relative';
+    // referencia.style.position = 'relative';
+
+    // areaRelatorio.classList.remove("fade");
+    // areaUmidade.classList.remove("fade");
+    // areaSuporte.classList.remove("fade");
+    // areaTemperatura.classList.add("fade");
+    temperatura.classList.remove("fade");
+    umidade.classList.add("fade");
+    tabelas.classList.remove("fade");
+    usuario.classList.remove("fade");
+    areaUmidade.classList.add("active");
+    areaTemperatura.classList.remove("active");
+    areaUsuario.classList.remove("active");
+    areaTabelas.classList.remove("active")
+  });
+
+  iconUmi3.addEventListener("click", function () {
+    usuario.style.display = "none";
+    temperatura.style.display = "none";
+    umidade.style.display = "block";
+    tabelas.style.display = "none";
+    // filtro.style.zIndex = '10';
+    // referencia.style.zIndex = '10';
+    // // filtro.style.position = 'relative';
+    // referencia.style.position = 'relative';
+
+    // areaRelatorio.classList.remove("fade");
+    // areaUmidade.classList.remove("fade");
+    // areaSuporte.classList.remove("fade");
+    // areaTemperatura.classList.add("fade");
+    temperatura.classList.remove("fade");
+    umidade.classList.add("fade");
+    tabelas.classList.remove("fade");
+    usuario.classList.remove("fade");
+    areaUmidade.classList.add("active");
+    areaTemperatura.classList.remove("active");
+    areaUsuario.classList.remove("active");
+    areaTabelas.classList.remove("active")
+  });
+
+  iconUmi4.addEventListener("click", function () {
+    usuario.style.display = "none";
+    temperatura.style.display = "none";
+    umidade.style.display = "block";
+    tabelas.style.display = "none";
+    // filtro.style.zIndex = '10';
+    // referencia.style.zIndex = '10';
+    // // filtro.style.position = 'relative';
+    // referencia.style.position = 'relative';
+
+    // areaRelatorio.classList.remove("fade");
+    // areaUmidade.classList.remove("fade");
+    // areaSuporte.classList.remove("fade");
+    // areaTemperatura.classList.add("fade");
+    temperatura.classList.remove("fade");
+    umidade.classList.add("fade");
+    tabelas.classList.remove("fade");
+    usuario.classList.remove("fade");
+    areaUmidade.classList.add("active");
+    areaTemperatura.classList.remove("active");
+    areaUsuario.classList.remove("active");
+    areaTabelas.classList.remove("active")
+  });
