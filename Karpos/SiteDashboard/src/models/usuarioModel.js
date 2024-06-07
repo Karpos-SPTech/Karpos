28ea36912a549be06a68b136c4e95ee7b93a9c32
Lucas-G-Carvalho <lucas.gcarvalho@sptech.school>
2024-06-07 14:26:25 -0300

    Inserindo a funcionalidade de criptografia de senhas

--- Karpos/SiteDashboard/src/models/usuarioModel.js
+++ Karpos/SiteDashboard/src/models/usuarioModel.js
@@ -3,7 +3,12 @@ var database = require("../database/config")
 function autenticar(email, senha) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
     var instrucaoSql = `
-        SELECT idUsuario, nome, email, senha, cpf, telefone, fkToken as empresaId FROM usuario WHERE email = '${email}' AND senha = '${senha}';
+         SELECT idUsuario, nome, email, senha, telefone, cpf, fkToken as empresaId,
+	CASE 
+	WHEN fun_valida_usuario('${email}','${senha}') = 1
+    THEN 'Login realizado com sucesso'
+    ELSE 'Login ou senha incorreta' END Validação
+ FROM usuario;
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
@@ -16,7 +21,7 @@ function cadastrar(nome, email, senha, documento, telefone, empresaId) {
     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
     //  e na ordem de inserção dos dados.
     var instrucaoSql = `
-        INSERT INTO usuario (nome, email, senha, cpf, telefone, fkToken) VALUES ('${nome}', '${email}', '${senha}', '${documento}','${telefone}','${empresaId}');
+        INSERT INTO usuario (nome, email, senha, telefone, cpf, fkToken) VALUES ('${nome}', '${email}', sha2('${senha}', 256), '${documento}','${telefone}','${empresaId}');
 
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
